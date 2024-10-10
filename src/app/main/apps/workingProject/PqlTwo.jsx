1daf1e682528fb02ccb6d1ac0191e93d0f9838ab
ShehrozTW <syedshehrozsohail@techwise-dev.com>
2024-10-10 14:30:56 +0500

    Custom checkbox handling

--- src/app/main/apps/workingProject/PqlTwo.jsx
+++ src/app/main/apps/workingProject/PqlTwo.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import menu from 'src/helpers/menu';
 
 const PqlTwo = () => {
-  return (
-    <div>
-      <h5>Content for PQL two</h5>
-      <p>This is where the details for PQL Two will go.</p>
-    </div>
-  )
-}
+  const initialState = {
+    transport: [],
+    geographic_area: [],
+  }
 
-export default PqlTwo
+  const [data, setData] = useState(initialState);
+
+  const handleCheckbox = category => e => {
+    const { name, checked } = e.target;
+    setData({ ...data, [category]: checked ? [...data[category], name] : data[category].filter((item) => item !== name), });
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+        <FormLabel>Assign responsibility</FormLabel>
+        <FormGroup>
+          {menu.transportActivity.map((item, index) => (
+            <FormControlLabel key={index} control={<Checkbox checked={data.transport.includes(item.value)} onChange={handleCheckbox('transport')} name={item.value} />} label={item.label} />
+          ))}
+        </FormGroup>
+      </FormControl>
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+        <FormLabel>Geographic area</FormLabel>
+        <FormGroup>
+          {menu.geographicArea.map((item, index) => (
+            <FormControlLabel key={index} control={<Checkbox checked={data.geographic_area.includes(item.value)} onChange={handleCheckbox('geographic_area')} name={item.value} />} label={item.label} />
+          ))}
+        </FormGroup>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default PqlTwo;
