0b8e8fc67ef20c142c8e1978958a33af22ef8bfe
Qadir Javed <108120593+qadirjaved1999@users.noreply.github.com>
2024-10-21 18:32:36 -0700

    QCM Table

--- src/app/main/pages/quick-check-methodology/QuickCheckMethodologyPageTable.jsx
+++ src/app/main/pages/quick-check-methodology/QuickCheckMethodologyPageTable.jsx
@@ -1,4 +1,5 @@
-import { FormControlLabel, Grid, Radio, RadioGroup, Table, TableBody, TableCell, TableRow, Typography } from '@mui/material'
+import { FormControlLabel, Grid, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import CustomTypography from 'src/helpers/custom-components/CustomTypography'
 
 const QuickCheckMethodologyPageTable = () => {
@@ -17,70 +18,46 @@ const QuickCheckMethodologyPageTable = () => {
                     color="primary"
                 />
             </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12}>
-
+            <Grid item xs={9}>
             </Grid>
-            <Grid container spacing={2}>
-
-                {/* Header Section */}
-                <Grid item xs={12}>
-                    <Typography variant="h6">Dimension 1: Regional Priority</Typography>
-                </Grid>
-
-                {/* Table Section */}
-                <Grid item xs={12}>
-                    <Table>
+            <Grid item xs={1.5}>
+                <Typography sx={{ textAlign: "center", fontWeight: "500" }}>Project Owner Score</Typography>
+            </Grid>
+            <Grid item xs={1.4}>
+                <Typography sx={{ textAlign: "center", fontWeight: "500" }}>Current Score (By SDM)</Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Dessert (100g serving)</TableCell>
+                                <TableCell align="right">Calories</TableCell>
+                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            </TableRow>
+                        </TableHead>
                         <TableBody>
-                            {/* Row for 'Level of regional interest' */}
-                            <TableRow>
-                                <TableCell>
-                                    <Typography variant="body1">Level of regional interest and political commitment</Typography>
-                                </TableCell>
-                                <TableCell>
-                                    <RadioGroup row>
-                                        <FormControlLabel value="5" control={<Radio />} label="5" />
-                                        <FormControlLabel value="10" control={<Radio />} label="10" />
-                                    </RadioGroup>
-                                </TableCell>
-                            </TableRow>
-
-                            {/* Additional Rows with Labels and Scores */}
-                            <TableRow>
-                                <TableCell>
-                                    <Typography variant="body2">Promotion of employment, intra-regional trade, and investment</Typography>
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="body2">5</Typography>
-                                </TableCell>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableCell>
-                                    <Typography variant="body2">Poverty reduction</Typography>
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="body2">5</Typography>
-                                </TableCell>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableCell>
-                                    <Typography variant="body2">Alignment with socioeconomic and environmental goals for Africa</Typography>
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="body2">7.5</Typography>
-                                </TableCell>
-                            </TableRow>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
+                                </TableRow>
+                            ))}
                         </TableBody>
                     </Table>
-                </Grid>
-
-                {/* Footer Section (Dimension 2) */}
-                <Grid item xs={12}>
-                    <Typography variant="h6">Dimension 2: Sector Readiness</Typography>
-                </Grid>
-
+                </TableContainer>
             </Grid>
+
         </Grid>
     )
 }
