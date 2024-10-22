8a0f29dc715d0b92125004897e296d110202acd5
Qadir Javed <108120593+qadirjaved1999@users.noreply.github.com>
2024-10-22 12:35:44 +0500

    QCM Table Changes

--- src/app/main/pages/quick-check-methodology/QuickCheckMethodologyPageTable.jsx
+++ src/app/main/pages/quick-check-methodology/QuickCheckMethodologyPageTable.jsx
@@ -1,6 +1,69 @@
-import { FormControlLabel, Grid, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import * as React from 'react';
+import { FormControlLabel, Grid, IconButton, Paper, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import CustomTypography from 'src/helpers/custom-components/CustomTypography'
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import RemoveCircleOutlineSharpIcon from '@mui/icons-material/RemoveCircleOutlineSharp';
+import Collapse from '@mui/material/Collapse';
+import { useState } from 'react';
+
+function createData(name, userValue, sdmValue) {
+    return {
+        name,
+        userValue,
+        sdmValue,
+    };
+}
+
+function Row(props) {
+    const { row } = props;
+    const [open, setOpen] = useState(false)
+
+    return (
+        <React.Fragment>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <RemoveCircleOutlineSharpIcon /> : <ControlPointIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell></TableCell>
+                <TableCell sx={{ width: "12%", textAlign: 'center' }}>{row.userValue}</TableCell>
+                <TableCell sx={{ width: "12%", textAlign: 'center' }}>{row.sdmValue}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{ margin: 1 }}>
+                            <Table size="small" aria-label="purchases">
+                                <TableBody>
+                                    <TableCell align="right">Salman</TableCell>
+                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}>Qadir javed</TableCell>
+                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}>Qadir javed</TableCell>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
+
+const rows = [
+    createData('Level of regional interest and political commitment', 5, 10),
+    createData('Promotion of employment, intra-regional trade and investment', 4, 7.5),
+    createData('Poverty reduction', 3, 5),
+    createData('Alignment with the socioeconomic and environmental sustainability goals for Africa', 6, 8),
+];
+
 
 const QuickCheckMethodologyPageTable = () => {
     return (
@@ -12,53 +75,37 @@ const QuickCheckMethodologyPageTable = () => {
                     display="block"
                     textAlign="center"
                     width="auto"
-                    padding="0px 0px 20px 0px"
+                    padding="0px 0px 50px 0px"
                     fontWeight="bold"
                     fontSize="18px"
                     color="primary"
                 />
             </Grid>
-            <Grid item xs={9}>
-            </Grid>
-            <Grid item xs={1.5}>
-                <Typography sx={{ textAlign: "center", fontWeight: "500" }}>Project Owner Score</Typography>
-            </Grid>
-            <Grid item xs={1.4}>
-                <Typography sx={{ textAlign: "center", fontWeight: "500" }}>Current Score (By SDM)</Typography>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12}>
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Dessert (100g serving)</TableCell>
-                                <TableCell align="right">Calories</TableCell>
-                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow
-                                    key={row.name}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell align="right">{row.calories}</TableCell>
-                                    <TableCell align="right">{row.fat}</TableCell>
-                                    <TableCell align="right">{row.carbs}</TableCell>
-                                    <TableCell align="right">{row.protein}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Grid>
-
+            <TableContainer component={Paper}>
+                <TableRow>
+                    <TableCell sx={{ width: "70%" }}></TableCell>
+                    <TableCell align="center" sx={{ width: "12%" }}>Project Owner score</TableCell>
+                    <TableCell align="center" sx={{ width: "12%" }}>Current Score (by SDM)</TableCell>
+                </TableRow>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell>Dimension 1: Regional priority</TableCell>
+                            <TableCell></TableCell>
+                            <TableCell align="center" sx={{ width: "12%" }}>7.5</TableCell>
+                            <TableCell align="center" sx={{ width: "12%" }}>5 &nbsp;</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <Row key={row.name} row={row} />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Grid>
+
     )
 }
 
