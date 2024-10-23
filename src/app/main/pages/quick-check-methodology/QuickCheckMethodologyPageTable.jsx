4f5666cf129ca62edb24ba9d77a7406ef9217893
Qadir Javed <108120593+qadirjaved1999@users.noreply.github.com>
2024-10-22 19:36:17 -0700

    optimize QCM

--- src/app/main/pages/quick-check-methodology/QuickCheckMethodologyPageTable.jsx
+++ src/app/main/pages/quick-check-methodology/QuickCheckMethodologyPageTable.jsx
@@ -1,140 +1,49 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { FormControlLabel, Grid, IconButton, Paper, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import CustomTypography from 'src/helpers/custom-components/CustomTypography'
+import { IconButton, Grid, Paper, Radio, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Collapse } from '@mui/material';
+import { Box, textAlign } from '@mui/system';
+import CustomTypography from 'src/helpers/custom-components/CustomTypography';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import RemoveCircleOutlineSharpIcon from '@mui/icons-material/RemoveCircleOutlineSharp';
-import Collapse from '@mui/material/Collapse';
 import languages from 'src/labels/index';
 import CustomRadioButton from 'src/helpers/custom-components/CustomRadioButton';
 
-// All Labls
 const label = languages.en;
 
-function createData(name, userValue, sdmValue) {
-    return {
-        name,
-        userValue,
-        sdmValue,
-    };
-}
-
-function Row(props) {
-    const { row } = props;
-    const [open, setOpen] = useState(false)
+const QuickCheckMethodologyPageTable = () => {
+    const [openRowIndex, setOpenRowIndex] = useState(null); // To control which row is open
 
     const initialState = {
         user_score: '',
         sdm_score: ''
-    }
-    const [data, setData] = useState(initialState)
+    };
+    const [data, setData] = useState(initialState);
+
     const handleChange = e => {
         const { name, value } = e.target;
         setData({ ...data, [name]: value });
-        console.log("Name => ", name + "Value => ", value);
-    }
-    return (
-        <React.Fragment>
-            <TableRow sx={{ '& > *': { border: "unset" }, backgroundColor: "#c0ddf6" }}>
-                <TableCell>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
-                    >
-                        {open ? <RemoveCircleOutlineSharpIcon /> : <ControlPointIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell component="th" scope="row" sx={{ width: "76%" }}>
-                    {row.name}
-                </TableCell>
-                <TableCell sx={{ width: "12%", textAlign: 'center' }}>{row.userValue}</TableCell>
-                <TableCell sx={{ width: "12%", textAlign: 'center' }}>{row.sdmValue}</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ padding: "0px 0px 0px 20px", backgroundColor: "#e4f0f9" }} colSpan={6}>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box>
-                            <Table size="small">
-                                <TableBody>
-                                    <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}><Radio /></TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}><Radio /></TableCell>
-                                </TableBody>
-                            </Table>
-                        </Box>
-                        <Box>
-                            <Table size="small">
-                                <TableBody>
-                                    <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}><Radio /></TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}><Radio /></TableCell>
-                                </TableBody>
-                            </Table>
-                        </Box>
-                        <Box>
-                            <Table size="small">
-                                <TableBody>
-                                    <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}><Radio /></TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}><Radio /></TableCell>
-                                </TableBody>
-                            </Table>
-                        </Box>
-                        <Box>
-                            <Table size="small">
-                                <TableBody>
-                                    <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}><Radio /></TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}><Radio /></TableCell>
-                                </TableBody>
-                            </Table>
-                        </Box>
-                        <Box>
-                            <Table size="small">
-                                <TableBody>
-                                    <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}>
-                                        <Radio
-                                            name='sdm_score'
-                                            value={data.sdm_score}
-                                            onChange={handleChange}
-                                        />
-                                    </TableCell>
-                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}>
-                                        <Radio
-                                            name='user_score'
-                                            value={data.user_score}
-                                            onChange={handleChange}
-                                        />
-                                    </TableCell>
-                                </TableBody>
-                            </Table>
-                        </Box>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </React.Fragment>
-    );
-}
+    };
 
-const rows = [
-    createData('Level of regional interest and political commitment', 5, 10),
-    createData('Promotion of employment, intra-regional trade and investment', 4, 7.5),
-    createData('Poverty reduction', 3, 5),
-    createData('Alignment with the socioeconomic and environmental sustainability goals for Africa', 6, 8),
-];
+    const rows = [
+        { name: 'Level of regional interest and political commitment', userValue: 5, sdmValue: 10 },
+        { name: 'Promotion of employment, intra-regional trade and investment', userValue: 4, sdmValue: 7.5 },
+        { name: 'Poverty reduction', userValue: 3, sdmValue: 5 },
+        { name: 'Alignment with the socioeconomic and environmental sustainability goals for Africa', userValue: 6, sdmValue: 8 }
+    ];
 
-const score = [
-    { value: 0 },
-    { value: 2.5 },
-    { value: 5 },
-    { value: 7.5 },
-    { value: 10 }
-]
+    const radioOptions = [
+        { label: "The project enjoys political commitment at national level, but there is no  evidence of its regional impact.", value: 0 },
+        { label: "The project is included in the PIDA PAP programme, but there is no  evidence of national-level commitment.", value: 2.5 },
+        { label: "The project is included in the PIDA PAP programme, and there is a  national-level commitment.", value: 5 },
+        { label: "The project has evidence of regional-level commitment, but is not included  in the PIDA PAP programme.", value: 7.5 },
+        { label: "The project enjoys regional-level commitment and it is included in the PIDA  PAP programme.", value: 10 }
 
-const QuickCheckMethodologyPageTable = () => {
+    ];
+
+    const handleRowClick = (index) => {
+        setOpenRowIndex(openRowIndex === index ? null : index); // Toggle open state for a specific row
+    };
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={12} sm={12} md={12} lg={12}>
@@ -151,11 +60,6 @@ const QuickCheckMethodologyPageTable = () => {
                 />
             </Grid>
             <TableContainer component={Paper}>
-                <TableRow>
-                    <TableCell sx={{ width: "70%" }}></TableCell>
-                    <TableCell align="center" sx={{ width: "12%" }}>Project Owner score</TableCell>
-                    <TableCell align="center" sx={{ width: "12%" }}>Current Score (by SDM)</TableCell>
-                </TableRow>
                 <Table aria-label="collapsible table">
                     <TableHead>
                         <TableRow sx={{ backgroundColor: "#2f2f2f" }}>
@@ -166,15 +70,100 @@ const QuickCheckMethodologyPageTable = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
-                            <Row key={row.name} row={row} />
+                        {rows.map((row, index) => (
+                            <React.Fragment key={row.name}>
+                                <TableRow sx={{ '& > *': { border: "unset" }, backgroundColor: "#c0ddf6" }}>
+                                    <TableCell>
+                                        <IconButton
+                                            aria-label="expand row"
+                                            size="small"
+                                            onClick={() => handleRowClick(index)}
+                                        >
+                                            {openRowIndex === index ? <RemoveCircleOutlineSharpIcon /> : <ControlPointIcon />}
+                                        </IconButton>
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" sx={{ width: "76%" }}>
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}>{row.sdmValue}</TableCell>
+                                    <TableCell sx={{ width: "12%", textAlign: 'center' }}>{row.userValue}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell style={{ padding: "0px 0px 0px 20px", backgroundColor: "#e4f0f9" }} colSpan={6}>
+                                        <Collapse in={openRowIndex === index} timeout="auto" unmountOnExit>
+                                            <Box>
+                                                <Table size="small">
+                                                {radioOptions.map((option, i) => (
+                                                            <TableRow key={i}>
+                                                                <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>
+                                                                    {option.label}
+                                                                </TableCell>
+                                                                <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}>
+                                                                    <CustomRadioButton
+                                                                        name="sdm_score"
+                                                                        value={data.sdm_score}
+                                                                        options={[
+                                                                            { value: '1' },
+                                                                        ]}
+                                                                        onChange={handleChange}
+                                                                    />
+                                                                </TableCell>
+                                                                <TableCell sx={{ width: "12%", textAlign: 'center' }}>
+                                                                    <CustomRadioButton
+                                                                        name="user_score"
+                                                                        value={data.user_score}
+                                                                        options={[
+                                                                            { value: '1'},
+                                                                        ]}
+                                                                        onChange={handleChange}
+                                                                    />
+                                                                </TableCell>
+                                                            </TableRow>
+                                                        ))}
+
+                                                    {/* <TableBody>
+                                                        <TableRow>
+                                                            <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
+                                                            <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}><Radio /></TableCell>
+                                                            <TableCell sx={{ width: "12%", textAlign: 'center' }}><Radio /></TableCell>
+                                                        </TableRow>
+                                                        <TableRow>
+                                                            <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
+                                                            <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}><Radio /></TableCell>
+                                                            <TableCell sx={{ width: "12%", textAlign: 'center' }}><Radio /></TableCell>
+                                                        </TableRow>
+                                                        <TableRow>
+                                                            <TableCell sx={{ padding: "6px 0px", width: "76%", textAlign: "start" }}>{label.noRegionalImpact}</TableCell>
+                                                            <TableCell sx={{ width: "12%", textAlign: 'center', backgroundColor: "white" }}>
+                                                                <Radio
+                                                                    name='sdm_score'
+                                                                    value={data.sdm_score}
+                                                                    onChange={handleChange}
+                                                                />
+                                                            </TableCell>
+                                                            <TableCell sx={{ width: "12%", textAlign: 'center' }}>
+                                                                <Radio
+                                                                    name='user_score'
+                                                                    value={data.user_score}
+                                                                    onChange={handleChange}
+                                                                />
+                                                            </TableCell>
+                                                        </TableRow>
+                                                    </TableBody> */}
+                                                </Table>
+                                            </Box>
+                                        </Collapse>
+                                    </TableCell>
+                                </TableRow>
+                            </React.Fragment>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
         </Grid>
-
-    )
+    );
 }
 
-export default QuickCheckMethodologyPageTable
+export default QuickCheckMethodologyPageTable;
+
+
